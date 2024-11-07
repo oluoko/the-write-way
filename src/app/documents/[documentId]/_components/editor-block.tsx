ee96674c6e927f6b33bfae6b6f254ccd91c018f3
oluoko <brianotieno586@gmail.com>
2024-11-07 04:35:54 +0300

    The Write Way: SingeDocumentPage Editor

---
+++ src/app/documents/[documentId]/_components/editor-block.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditorBlock = () => {
+  return <div>EditorBlock</div>;
+};
+
+export default EditorBlock;
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,25 @@
-const DocumentPage = () => {
-  return <div>DocumentPage</div>;
+import { db } from "@/utils/db";
+import EditorBlock from "./_components/editor-block";
+
+interface SingleDocumentPageProps {
+  params: {
+    documentId: string;
+  };
+}
+
+const SingleDocumentPage = async ({ params }: SingleDocumentPageProps) => {
+  const document = await db.document.findUnique({
+    where: {
+      id: params.documentId,
+    },
+  });
+
+  return (
+    <div>
+      <h1>{document?.title}</h1>
+      <EditorBlock />
+    </div>
+  );
 };
 
-export default DocumentPage;
+export default SingleDocumentPage;
